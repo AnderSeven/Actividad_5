717387a65df4240e90b7bcd9efcc18b58cfe221c
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-07-11 20:59:35 -0600

    Creacion del menu

---
+++ Actividad_5.py
@@ -0,0 +1,9 @@
+lista = []
+class Estudiante:
+    def __init__(self, nombre, carne, carrera, nota_final):
+
+print("---Menu---")
+print("1. Registrar estudiante")
+print("2. Mostrar la lista de estudiantes")
+print("3. Buscar estudiante")
+print("4. Calcular el promedio de todos los estudiantes")
